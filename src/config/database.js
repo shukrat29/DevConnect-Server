65741aef627f6fd959a528d7e4c781b696935633
Shukra Tamang <shukratamang.npl@gmail.com>
2025-01-29 12:05:37 +1100

    update

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+console.log("MONGODB_URL:", process.env.MONGODB_URL);
 
 const connectDB = async () => {
   await mongoose.connect(process.env.MONGODB_URL);
